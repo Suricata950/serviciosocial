2d8d5581dafe74f72725096adb6c20010eccb6eb
EDUARDO ZAHIR RUIZ COLIN <zahir_ruiz@hotmail.com>
2025-04-04 10:31:50 -0600

    entidad Alumno agregada

---
+++ src/main/java/com/example/demo/entity/Alumno.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="alumno")
+public class Alumno {
+    @Id
+    private String id;
+    private String nombre;
+    @Column(name = "apellido_paterno")
+    private String apellidoPaterno;
+    private int edad;
+    private String correo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
