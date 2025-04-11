425a906111de249af86c1d9f38103ac7b5c44b21
Gerardo <Gerardo462@users.noreply.github.com>
2025-04-11 10:06:30 -0600

    Agrego archivos iniciales

---
+++ src/main/java/proyecto/ServicioSocial/ServicioSocialApplication.java
@@ -0,0 +1,13 @@
+package proyecto.ServicioSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioSocialApplication.class, args);
+	}
+
+}
