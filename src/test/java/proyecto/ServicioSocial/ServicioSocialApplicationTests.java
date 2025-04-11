5f9ff6794c1301bc4cd63cfd30e0ed7fcef498e4
zahir <zahir_ruiz@hotmail.com>
2025-04-11 09:14:09 -0600

    Primer commit del proyecto

---
+++ src/test/java/proyecto/ServicioSocial/ServicioSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.ServicioSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
