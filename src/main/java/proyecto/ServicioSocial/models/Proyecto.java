aadb04ab177c915b0095a7ba90930a53c733fa04
Gerardo <Gerardo462@users.noreply.github.com>
2025-04-16 12:18:19 -0600

    Prueba de cambio

---
+++ src/main/java/proyecto/ServicioSocial/models/Proyecto.java
@@ -0,0 +1,62 @@
+package proyecto.ServicioSocial.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Proyecto {
+
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProyecto;
+
+    private String nombreProyecto;
+    private String descripcion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    // Getters y Setters
+    public Long getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(Long idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public String getNombreProyecto() {
+        return nombreProyecto;
+    }
+
+    public void setNombreProyecto(String nombreProyecto) {
+        this.nombreProyecto = nombreProyecto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+}
